2d9a9560f792863ae8b48de713c90053e7efee2d
davidgamaliel <davidgamaliel.sihombing@gmail.com>
2017-04-23 15:28:06 +0700

    test

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,6 +5,12 @@ return array(
 	// 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
+	/*'connectionString' => 'pgsql:host=siud.gandafitrandia-380d.aivencloud.com;port=23879;dbname=defauldb',
+	'emulatePrepare' => true,
+	'username' => 'avnadmin',
+	'password' => 'tgdpqpmdpmr9w5n6',
+	'charset' => 'utf8',*/
+
 	'connectionString' => 'pgsql:host=localhost;port=5432;dbname=siud_db',
 	'emulatePrepare' => true,
 	'username' => 'postgres',
